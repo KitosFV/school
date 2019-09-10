e5e10399167bf11ee67d960a4b29fd5617d0aa0a
KitosFV <kitos200387@gmail.com>
2019-09-10 13:38:03 +0300

    328 beta

--- acmp328(nc)/main.cpp
+++ acmp328(nc)/main.cpp
@@ -5,7 +5,13 @@ int main(){
     ifstream in("input.txt");
     ofstream out("output.txt");
 
+    int n, s = 0;
+    in >> n;
+    s+=2*n;
 
+    for(int j = 0;j<n;j++)
+        for(int i = j;i<=n;i++)s+=i+j;
 
+    out << s;
     return 0;
 }
