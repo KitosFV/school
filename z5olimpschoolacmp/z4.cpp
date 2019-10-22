c178b67953d369479ec4f283a08af4c6ea90e1b1
KitosFV <kitos200387@gmail.com>
2019-10-22 13:21:08 +0300

    re

---
+++ z5olimpschoolacmp/z4.cpp
@@ -0,0 +1,35 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+
+string gen(int n,int t,string b,string g){
+    string a[n],k = "";
+    for(int i=0;i<n;i++){
+        a[i]=b+g;
+    }
+    if(t>0)a[n-1]+=b;
+    for(int i=0;i<t;i++){
+        a[i+1]+=b;
+    }
+    for(int i=0;i<n;i++){
+        k+=a[i];
+    }
+    return k;
+}
+
+int main(){
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int m, n;in >> m >> n;
+    string b="B",g="G";
+
+    if(m>n){swap(m,n);swap(b,g);}
+
+    if(n <= 2*m){
+        out << gen(m,n-m,b,g);
+    }else{
+        out << "No solution" << endl;
+    }
+
+    return 0;
+}
